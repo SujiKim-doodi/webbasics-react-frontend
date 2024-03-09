4044c1df28fef773dbf4be0d117a96047bd3baaf
SujiKim <136331819+DooDI-SujiKim@users.noreply.github.com>
2024-03-09 17:38:05 +0900

    feat: add JavaScript explanations and variable declarations

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+// log: 정보 기록 및 출력
 console.log("Hello Codeit");
+
+/* 
+    자료형 data type
+    정의: 프로그레밍에서 쓰이는 데이터의 종류
+    예시: 정수, 실수, 문자열, 부울형, 리스트, 튜플, 딕셔너리, 집합 등
+*/
+
+/*
+    추상화 abstraction
+    정의: 복잡한 것들을 목적에 맞게 단순화한 것. 핵심만
+    예시: 요약, 지도 등
+*/
+
+/* 
+    변수 variable
+    정의: 값의 저장소 -> 선언 & 할당(초기화)
+    특징: 코드 이해 쉬움. 할당할 때는 '='등호 하나와 함께 쓰인다.
+    *=: 할당 연산자. 우변을 좌변에 할당한다는 의미.
+*/
+
+// 변수 선언
+let coffeePrice = 3000;
+let teaPrice = 2000;
+let juicePrice = 4000;
+
+console.log(coffeePrice*2); // 6000
+console.log(coffeePrice+teaPrice); // 5000
+console.log(juicePrice*2+coffeePrice+teaPrice); // 13000
